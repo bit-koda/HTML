4f08ba6e11925cc032999af28c6507885694bbe1
Sherukoda <62921253+vishal4234@users.noreply.github.com>
2025-05-22 13:22:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const content = {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dark Mode</title>
+    <title>HTML Editor</title>
 </head>
 <body>
     <h1>Hello, World!</h1>
